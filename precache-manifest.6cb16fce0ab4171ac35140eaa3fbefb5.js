2d9258b04f7fe655527846d11e909dfd4c8b9884
Suman <sumanizrhythm@gmail.com>
2019-12-14 22:46:58 +0545

    Updates

---
+++ precache-manifest.6cb16fce0ab4171ac35140eaa3fbefb5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "613cbeeca4c30df9132c68a2df6ce995",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "7fdceb9e9e70014d5b9e",
+    "url": "/weather-react-app/static/css/main.7ac8e65f.chunk.css"
+  },
+  {
+    "revision": "dcb5da346fa0b77320b0",
+    "url": "/weather-react-app/static/js/2.2b545bda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-react-app/static/js/2.2b545bda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7fdceb9e9e70014d5b9e",
+    "url": "/weather-react-app/static/js/main.5920acfb.chunk.js"
+  },
+  {
+    "revision": "4993be27082a4ee16460",
+    "url": "/weather-react-app/static/js/runtime-main.f7b90370.js"
+  },
+  {
+    "revision": "0f0cb234b0e04db2b3bbc6233988ba6d",
+    "url": "/weather-react-app/static/media/drizzle.0f0cb234.gif"
+  },
+  {
+    "revision": "aa895126d1033a2d751cd4928c69feda",
+    "url": "/weather-react-app/static/media/fog.aa895126.gif"
+  },
+  {
+    "revision": "1bfccb37e12bf74bd56b607903f15ba7",
+    "url": "/weather-react-app/static/media/rain.1bfccb37.gif"
+  },
+  {
+    "revision": "526c87ab14163f65cd6da978b1693eb5",
+    "url": "/weather-react-app/static/media/snow.526c87ab.gif"
+  },
+  {
+    "revision": "78e15a7a1ba1e5497d967d7a9d6edf2b",
+    "url": "/weather-react-app/static/media/sunny.78e15a7a.gif"
+  },
+  {
+    "revision": "425b811084dd2421c1df0fbe7576d883",
+    "url": "/weather-react-app/static/media/thunderstrom.425b8110.gif"
+  },
+  {
+    "revision": "b1c5efa1117e6169675ffe920927a32b",
+    "url": "/weather-react-app/static/media/weather.b1c5efa1.gif"
+  }
+]);
